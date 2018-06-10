512f132acfd6762425254e42ee32373e2ca4ae99
ares97 <digitalares97@gmail.com>
2018-06-10 17:56:33 +0200

    Enable shooting on client side

--- core/src/karabaka/game/client/entities/Player.java
+++ core/src/karabaka/game/client/entities/Player.java
@@ -3,21 +3,26 @@ package karabaka.game.client.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import karabaka.game.client.handlers.ClientMoveHandlerImpl;
+import karabaka.game.client.handlers.ClientShootHandlerImpl;
 import karabaka.game.client.handlers.MoveHandler;
+import karabaka.game.client.handlers.ShootHandler;
 import karabaka.game.client.utils.Direction;
 
 public class Player {
 
     private Tank tank;
     private MoveHandler moveHandler;
+    private ShootHandler shootHandler;
 
-    public Player(Tank tank, MoveHandler moveHandler) {
+    public Player(Tank tank, MoveHandler moveHandler, ShootHandler shootHandler) {
         this.tank = tank;
         this.moveHandler = moveHandler;
+        this.shootHandler = shootHandler;
     }
 
     public Player() {
         moveHandler = new ClientMoveHandlerImpl();
+        shootHandler = new ClientShootHandlerImpl();
     }
 
     public void handlePlayerInput() {
@@ -29,6 +34,9 @@ public class Player {
             move(Direction.DOWN);
         } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             move(Direction.RIGHT);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            shootHandler.shoot(tank.getDirection(), tank);
         }
     }
 
