a114ca93bbdef1e26f232400b7b9fb82ba7f557d
ares97 <digitalares97@gmail.com>
2018-06-10 11:43:38 +0200

    Add direction for tank

--- core/src/karabaka/game/client/entities/Tank.java
+++ core/src/karabaka/game/client/entities/Tank.java
@@ -2,14 +2,18 @@ package karabaka.game.client.entities;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import karabaka.game.client.utils.Direction;
 
 public class Tank extends Rectangle {
 
     private Texture texture;
+    private Direction direction;
 
-    public Tank(float x, float y) {
+    public Tank(float x, float y, Direction direction) {
         this.x = x;
         this.y = y;
+        this.direction = direction;
+
         texture = new Texture("core/assets/Tank_up.png");
     }
 
---
+++ core/src/karabaka/game/client/utils/Direction.java
@@ -0,0 +1,18 @@
+package karabaka.game.client.utils;
+
+public enum Direction {
+    LEFT(1),
+    RIGHT(2),
+    UP(3),
+    DOWN(4);
+
+    Direction(int value) {
+        this.value = value;
+    }
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+}
