ccd2098955fe88407347184449880f30e8921587
ares97 <digitalares97@gmail.com>
2018-06-10 11:38:38 +0200

    Add bullet entity

---
+++ core/src/karabaka/game/client/entities/Bullet.java
@@ -0,0 +1,15 @@
+package karabaka.game.client.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Bullet extends Rectangle {
+
+    private Texture texture;
+
+    public Bullet(int x, int y){
+        this.x = x;
+        this.y = y;
+        texture = new Texture("core/assets/bullet.png");
+    }
+}
