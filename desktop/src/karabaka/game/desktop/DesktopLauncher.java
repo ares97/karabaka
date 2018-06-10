3c1249816298e26fabf52fc6d370d935111caa1c
ares97 <digitalares97@gmail.com>
2018-06-10 11:25:40 +0200

    init

---
+++ desktop/src/karabaka/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package karabaka.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import karabaka.game.KarabakaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KarabakaGame(), config);
+	}
+}
