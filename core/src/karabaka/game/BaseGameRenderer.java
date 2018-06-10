50f584c339b6d1d0bcf9189382b0d1d6dc8e082e
ares97 <digitalares97@gmail.com>
2018-06-10 12:48:42 +0200

    extract rendering fragment to base class

---
+++ core/src/karabaka/game/BaseGameRenderer.java
@@ -0,0 +1,51 @@
+package karabaka.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import karabaka.game.client.EntityContainer;
+import karabaka.game.client.entities.Bullet;
+import karabaka.game.client.entities.Tank;
+
+public class BaseGameRenderer extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        renderEntities();
+        batch.end();
+    }
+
+    private void renderEntities() {
+        for (Tank tank : EntityContainer.instance.getTanks())
+            batch.draw(tank.getTexture(), tank.x, tank.y);
+
+        for (Bullet bullet : EntityContainer.instance.getBullets())
+            batch.draw(bullet.getTexture(), bullet.x, bullet.y);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        disposeEntityTextures();
+    }
+
+    private void disposeEntityTextures() {
+        for (Tank tank : EntityContainer.instance.getTanks())
+            tank.getTexture().dispose();
+        for (Bullet bullet : EntityContainer.instance.getBullets())
+            bullet.getTexture().dispose();
+    }
+}
--- core/src/karabaka/game/client/KarabakaClient.java
+++ core/src/karabaka/game/client/KarabakaClient.java
@@ -1,52 +1,14 @@
 package karabaka.game.client;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import karabaka.game.client.entities.Bullet;
-import karabaka.game.client.entities.Player;
+import karabaka.game.BaseGameRenderer;
 import karabaka.game.client.entities.Tank;
 import karabaka.game.client.utils.Direction;
 
-public class KarabakaClient extends ApplicationAdapter {
-
-    private SpriteBatch batch;
+public class KarabakaClient extends BaseGameRenderer {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-    }
-
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        renderEntities();
-        batch.end();
-    }
-
-    private void renderEntities() {
-        for (Tank tank : EntityContainer.instance.getTanks())
-            batch.draw(tank.getTexture(), tank.x, tank.y);
-
-        for (Bullet bullet : EntityContainer.instance.getBullets())
-            batch.draw(bullet.getTexture(), bullet.x, bullet.y);
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        disposeEntityTextures();
-    }
-
-    private void disposeEntityTextures() {
-        for (Tank tank : EntityContainer.instance.getTanks())
-            tank.getTexture().dispose();
-        for (Bullet bullet : EntityContainer.instance.getBullets())
-            bullet.getTexture().dispose();
+        super.create();
+        EntityContainer.instance.addTank(new Tank(40,70,Direction.LEFT));
     }
 }
