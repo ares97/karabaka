8eaaaa650ce3264e1ee297d634f964449ed271bc
ares97 <digitalares97@gmail.com>
2018-06-11 18:48:09 +0200

    change tank size

--- core/src/karabaka/game/client/utils/GameSettings.java
+++ core/src/karabaka/game/client/utils/GameSettings.java
@@ -6,7 +6,7 @@ public class GameSettings {
     public static final int BULLET_MOVE_SIZE = 6;
 
     public static final long BULLET_COOLDOWN_MS = 600;
-    public static final float TANK_SIZE = 64.0f;
+    public static final float TANK_SIZE = 36.0f;
     public static final float BULLET_SIZE = 32.0f;
 
     public static final int GAME_WIDHT = 640;
